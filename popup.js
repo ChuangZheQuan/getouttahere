f590a854a33782a9972896de71a0bf2f7ff5273c
ZQ <zhequan7chuang@gmail.com>
2020-12-23 14:12:46 +0800

    first commit

---
+++ popup.js
@@ -0,0 +1,90 @@
+
+function increaseValue() {
+    var value = parseInt(document.getElementById("number").value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById("number").value = value;
+    storage.remove("time", () => {
+        storage.set({"time": value});
+    })
+    location.reload();
+}
+  
+function decreaseValue() {
+    var value = parseInt(document.getElementById("number").value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById("number").value = value;
+    storage.remove("time", () => {
+        storage.set({"time": value});
+    })
+    location.reload();
+}
+
+function redirect(page){
+    if (page == "websites"){
+        //redirect to websites page
+        location.href = "/screens/websites.html"
+    } else {
+        console.log("error");
+    }
+}
+
+function pause_resume(){
+    // 0 is for pause, 1 is for resume
+    let val = document.getElementById("pause-resume-label").value;
+    if (val === 0) {
+        document.getElementById("pause-resume-label").value = 1;
+        document.getElementById("pause-resume-button").className = "fas fa-play";
+        storage.remove("pause-resume-label", () => {
+            storage.set({"pause-resume-label": 1})
+        })
+    } else if (val === 1) {
+        document.getElementById("pause-resume-label").value = 0;
+        document.getElementById("pause-resume-button").className = "fas fa-pause";
+        storage.remove("pause-resume-label", () => {
+            storage.set({"pause-resume-label": 0})
+        })
+        // pause background.js
+        
+    }
+    location.reload();
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("decrease").addEventListener("click", decreaseValue);
+    document.getElementById("increase").addEventListener("click", increaseValue);
+    document.getElementById("websites").addEventListener("click", () => redirect('websites'));
+    document.getElementById("pause-resume").addEventListener("click", pause_resume);
+});
+
+//WHEN PAGE IS BEING RELOADED
+document.addEventListener("DOMContentLoaded", function(){
+    storage.get("time", (result) => {
+        if (result["time"] === undefined){
+            document.getElementById("number").value = 5;
+        } else {
+            console.log("result equals: " + result["time"]);
+            document.getElementById("number").value = result["time"]; 
+        }
+    })
+    
+    // 1 is paused, 0 is resumed
+    storage.get("pause-resume-label", (result) => {
+        console.log(result["pause-resume-label"]);
+        if (result["pause-resume-label"] === 1){ 
+            //Initially paused, change to resume
+            document.getElementById("pause-resume-label").value = 1;
+            document.getElementById("pause-resume-button").className = "fas fa-play";
+        } else if (result["pause-resume-label"] === 0){
+            //Initially resume, change to paused
+            document.getElementById("pause-resume-label").value = 0;
+            document.getElementById("pause-resume-button").className = "fas fa-pause";
+        } else {
+            document.getElementById("pause-resume-label").value = 1;
+            document.getElementById("pause-resume-button").className = "fas fa-pause";
+        }
+    })
+})
+
---
+++ websites.js
@@ -0,0 +1,116 @@
+function redirect(page){
+    if (page == "popup") {
+        //return to popup page
+        location.href = "/screens/popup.html";
+    } else {
+        console.log("error");
+    }
+}
+
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+}
+
+function add_website_popup(){
+    //create popup to ask for website url
+    let input_url = prompt("Enter a website");
+    chrome.storage.sync.get(null, (result) => {
+        obj = Object.keys(result);
+        var index = obj.length;
+        if (validURL(input_url)){
+            chrome.storage.sync.set({[index] : input_url});
+            location.reload();
+        } else if (input_url === null){
+            return;
+        } else {
+            alert("Invalid URL");
+            return;
+        }
+    });
+}
+
+function delete_website(index){
+    try{
+        chrome.storage.sync.remove([index]);
+        location.reload();
+    } catch (e) {
+        console.log("No such data!");
+    }
+    
+}
+        
+function load_naughty_list(tableBody){
+    //get naughty list from chrome storage
+    try{
+        chrome.storage.sync.get(null, function (result){
+            populateTable(tableBody, result);
+        });
+    } catch (e) {
+        console.warn("Could not load data!");
+    }
+}
+
+function populateTable(tableBody, data){
+    //clears out table
+    while (tableBody.firstChild){
+        tableBody.removeChild(tableBody.firstChild);
+    }
+    //populate table
+    let obj = Object.entries(data)
+    var index = 1;
+    for (row of obj) {
+        if (!Number.isInteger(row[1])){
+            const tr = document.createElement("tr");
+            const index_td = document.createElement("td");
+            index_td.textContent = index;
+    
+            const url_td = document.createElement("td"); 
+            url_td.textContent = row[1];
+    
+            const del_td = document.createElement("td");
+            const del_btn = document.createElement("input");
+            del_btn.type = "button";
+            del_btn.className = "btn btn-danger";
+            del_btn.value = "Delete";
+            del_btn.id = row[0];
+            del_btn.onclick = () => delete_website(del_btn.id);
+            del_td.appendChild(del_btn);
+    
+            tr.appendChild(index_td);
+            tr.appendChild(url_td);   
+            tr.appendChild(del_td);
+            tableBody.appendChild(tr);
+            index += 1;
+        } else {}
+    }
+};
+
+function clear_websites_table(){
+    if (confirm("Remove all websites from naughty list?")){
+        chrome.storage.sync.clear(() => console.log('cleared'));
+        location.reload();
+    } else {}
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let tableBody = document.querySelector("tbody");
+    load_naughty_list(tableBody);
+    //Listener to go back to main page
+    document.getElementById("popup").addEventListener("click", () => redirect('popup'));
+
+    //Listener for the Add Button
+    document.getElementById("add-btn").addEventListener("click", add_website_popup);
+
+    //Listener to delete all websites from chrome storage
+    document.getElementById("clear-btn").addEventListener("click", clear_websites_table);
+});
+
+
+ 
+
